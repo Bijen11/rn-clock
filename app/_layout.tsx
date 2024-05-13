ef2779f43fa0c0f405f8d666b2d34ea7d6a6611d
Bijen11 <shahibijen@gmail.com>
2024-05-13 15:01:49 +0545

    moti animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { MotiView } from 'moti';
 import 'react-native-reanimated';
+import { useEffect } from 'react';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import {Feather} from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
+import { View,Text, StyleSheet } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { View,Text } from 'react-native';
+import { Easing } from 'react-native-reanimated';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -26,12 +29,46 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
+  
+  const hiddenCircle = (index:number) => {
+  return(
+    <MotiView 
+    from={{opacity:.5, scale : 1}}
+    animate={{opacity:0 , scale :4 }}
+    key={index} style={[StyleSheet.absoluteFillObject,style.dot ]} 
+    transition={{type: 'timing', duration: 2000, easing: Easing. out (Easing.ease), loop : true , delay:index* 400 , repeatReverse:false }}/>
+
+  )  
+  }
 
   return (
     
-      <View style={{flex:1,backgroundColor:'white'}}>
-     <Text>hey</Text>
-     </View>
+    <View style={{flex:1, alignItems:'center', justifyContent:'center' ,backgroundColor:'white'}}>   
+    <View style={[style.dot,style.center]}>
+     
+      {[...Array(3).keys()].map((index)=>{
+        return(hiddenCircle(index))
+      })}
+      <MotiView 
+        from={{ rotate: '2deg' }}
+        animate={{ rotate: '-2deg' }}
+        transition={{ type: 'timing', loop: true, repeat: Infinity }}>
+        <Feather name='phone-outgoing' size= {32} color='#FFF'/>
+      </MotiView>
+      
+    </View>
+ </View>
   
   );
 }
+
+const style= StyleSheet.create({
+  dot:{
+    width:100,
+    height:100,
+    borderRadius:100,
+    backgroundColor:'#6E01EF'
+  },
+  center:{alignItems:'center' , justifyContent:'center'}
+  
+})
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { useEffect } from 'react';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import {Feather} from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { View,Text } from 'react-native';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -24,12 +25,27 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
+  
   return (
-    
-      <View style={{flex:1,backgroundColor:'white'}}>
-     <Text>fuck</Text>
+      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>   
+        <View style={[styles.dot,style.center]}>
+          <Feather name='phone-outgoing' size= {32} color='#FFF'/>
+          <Text>here</Text>
+        </View>
      </View>
   
   );
 }
+
+const style= StyleSheet.create({
+  dot:{
+    width:100,
+    height:100,
+    borderRadius:100,
+    backgroundColor:'#6E01EF'
+  },
+  center:{alignItems:'center' , justifyContent:'center'}
+  
+})
+
+
