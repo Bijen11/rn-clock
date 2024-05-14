8d525a833b9d2f85120d3a556a8ced01d717e2ed
Bijen11 <shahibijen@gmail.com>
2024-05-14 13:58:08 +0545

    clock design implemented

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
-import { MotiView } from 'moti';
+import {MotiView} from 'moti';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
-import { useEffect } from 'react';
-import { Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
+import {Easing} from 'react-native-reanimated';
+import {View, Dimensions, StyleSheet} from 'react-native';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+
+import {Stack} from 'expo-router';
+import {useFonts} from 'expo-font';
 import {Feather} from '@expo/vector-icons';
 import * as SplashScreen from 'expo-splash-screen';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 
-import { View,Text, StyleSheet } from 'react-native';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Easing } from 'react-native-reanimated';
+import {useColorScheme} from '@/hooks/useColorScheme';
+
+const {width} = Dimensions.get('screen');
+
+const SIZE = width * 0.9;
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,46 +34,85 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-  
-  const hiddenCircle = (index:number) => {
-  return(
-    <MotiView 
-    from={{opacity:.5, scale : 1}}
-    animate={{opacity:0 , scale :4 }}
-    key={index} style={[StyleSheet.absoluteFillObject,style.dot ]} 
-    transition={{type: 'timing', duration: 2000, easing: Easing. out (Easing.ease), loop : true , delay:index* 400 , repeatReverse:false }}/>
-
-  )  
-  }
 
   return (
-    
-    <View style={{flex:1, alignItems:'center', justifyContent:'center' ,backgroundColor:'white'}}>   
-    <View style={[style.dot,style.center]}>
-     
-      {[...Array(3).keys()].map((index)=>{
-        return(hiddenCircle(index))
-      })}
-      <MotiView 
-        from={{ rotate: '2deg' }}
-        animate={{ rotate: '-2deg' }}
-        transition={{ type: 'timing', loop: true, repeat: Infinity }}>
-        <Feather name='phone-outgoing' size= {32} color='#FFF'/>
-      </MotiView>
-      
+    <View style={style.container}>
+      <View style={[style.bigQuadran]} />
+      <View style={[style.mediumQuadran]} />
+      <View style={[style.smallQuadran]} />
+
+      <View style={[style.mover]}>
+        <View style={[style.hours]} />
+      </View>
+
+      <View style={[style.mover]}>
+        <View style={[style.minutes]} />
+      </View>
+
+      <View style={[style.mover]}>
+        <View style={[style.seconds]} />
+      </View>
     </View>
- </View>
-  
   );
 }
 
-const style= StyleSheet.create({
-  dot:{
-    width:100,
-    height:100,
-    borderRadius:100,
-    backgroundColor:'#6E01EF'
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  center:{alignItems:'center' , justifyContent:'center'}
-  
-})
+  mover: {
+    width: SIZE,
+    height: SIZE,
+    position: 'absolute',
+    alignItems: 'center',
+    borderRadius: SIZE / 2,
+    justifyContent: 'flex-start',
+  },
+  hours: {
+    width: 4,
+    height: '35%',
+    borderRadius: 4,
+    marginTop: '15%',
+    position: 'absolute',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  minutes: {
+    width: 3,
+    height: '45%',
+    borderRadius: 3,
+    marginTop: '5%',
+    position: 'absolute',
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  },
+  seconds: {
+    width: 2,
+    height: '50%',
+    borderRadius: 2,
+    position: 'absolute',
+    backgroundColor: 'rgba(277,71,134,1)',
+  },
+  bigQuadran: {
+    width: SIZE * 0.8,
+    height: SIZE * 0.8,
+    position: 'absolute',
+    borderRadius: SIZE * 0.4,
+    backgroundColor: 'rgba(200,200,200,0.2)',
+  },
+  mediumQuadran: {
+    width: SIZE * 0.5,
+    height: SIZE * 0.5,
+    position: 'absolute',
+    borderRadius: SIZE * 0.25,
+    backgroundColor: 'rgba(200,200,200,0.4)',
+  },
+  smallQuadran: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    position: 'absolute',
+    backgroundColor: 'rgba(277,71,134,1)',
+  },
+});
